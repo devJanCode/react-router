dc5397fb9804d584545ea937dfa545da56787f83
Jan <jaspes@orange.fr>
2019-04-09 00:04:24 +0200

    creation de react-router-challenge

---
+++ src/History.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class History extends Component {
+    render() {
+        return(
+            <div className="History">
+                <h1>Notre Histoire</h1>
+            </div>
+        );
+    }
+}
+
+export default History;
